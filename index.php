5f4eec18e23e20de0af20a9568aeeae5f00d8d0d
DroidStudioAS <as.droiddevteam@gmail.com>
2024-06-14 19:46:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="app/style.css"/>
+    <title>BlogBook</title>
+</head>
+<body>
+    <div class="loginRegSwitch">
+        <div>Login</div>
+        <div>Register</div>
+    </div>
+   <div id="loginContainer" class="loginContainer">
+
+   </div>
+
+   <div id="registerContainer" class="registerContainer">
+
+
+   </div>
+   
+</body>
+</html>
