8e928e4fe31294f1eccf558b2a5280bc0ddfdad9
DroidStudioAS <as.droiddevteam@gmail.com>
2024-06-16 12:57:13 +0200

    added the unfollow logic to remove someone from friends

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -34,14 +34,33 @@ class UserController{
         //get a db instance
         $database = new Database();
         $conn=$database->getConnection();
-        //prepare statement
-        $stmt=$conn->prepare("INSERT INTO friends (user_id, friend_id) VALUES(?,?)");
-        $stmt->bind_param("ii",$userId,$friendId);
-        //execute statement and http differ
-        $inserted = $stmt->execute();
-        if(!$inserted){
-            //add message to session for error
+        //check if is already in friends
+        $stmt = $conn->prepare("SELECT id FROM friends WHERE user_id=? AND friend_id=?");
+        $stmt->bind_param('ii', $userId,$friendId);
+        $stmt->execute();
+        $result=$stmt->get_result()->fetch_assoc();
+        $stmt->close();
+        //if result === null, he is not a friend
+        if($result==null){
+            $stmt=$conn->prepare("INSERT INTO friends (user_id, friend_id) VALUES(?,?)");
+            $stmt->bind_param("ii",$userId,$friendId);
+            //execute statement and http differ
+            $inserted = $stmt->execute();
+            if(!$inserted){
+                //add message to session for error
+            }
+        }else{
+            //remove him
+            $stmt=$conn->prepare("DELETE FROM friends WHERE user_id=? AND friend_id=?");
+            $stmt->bind_param("ii",$userId,$friendId);
+            //execute statement and http differ
+            $deleted = $stmt->execute();
+            if(!$deleted){
+                //add message to session for error
+            }
         }
+
+       
         header('Location: ' . $_SERVER['HTTP_REFERER']);
         
 
