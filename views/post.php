0a7343e4865ae1c851890d20207d110e8ee14f1b
DroidStudioAS <as.droiddevteam@gmail.com>
2024-06-16 12:49:06 +0200

    styled add comment form and added an unfollow button

--- views/post.php
+++ views/post.php
@@ -21,7 +21,41 @@
         echo  "<p>" ."\"  " . $post->getBody() ."  \"". "</p>";
         echo "<p>By: <span class='postAuthor'>" .  $post->getUsernameById($post->getUserId()) . "</span></p>";
         echo "</div>";
+        ?>
 
+        <form method="post", action="../app/comments/add_comment.php">
+            <input class="authInput" type="text" placeholder="Leave a comment" name="body">
+            <?php 
+                echo "<input type='hidden' name='postId' value='" .  $_GET["post_id"] . "'>";
+            ?>
+            <input class="inputSubmit" type="submit" value="+">
+        </form>
+            
+         <form method="post", action="../app/users/add_friend.php">
+         <?php 
+         require_once "../models/User.php";
+         require_once "../models/Post.php";
+
+         session_start();
+         $user = unserialize($_SESSION['user']);
+         $friends = $user->getFriends($user->getId());
+         
+         $post = Post::getPostById($_GET["post_id"]);
+         $userId = $post->getUserId();
+         if(in_array($userId, $friends) || $userId==$user->getId()){
+            if($userId!==$user->getId()){
+                 echo '<input class="followTrigger" type="submit" value="Unfollow post author">';
+            }
+            }else{
+            echo '<input class="followTrigger" type="submit" value="Follow post author">';
+
+         }
+         echo "<input type='hidden' name='userId' value='" . $userId . "'>"
+         ?>
+        </form>
+
+
+        <?php
         echo "<p class='header'>";
         echo "Comments";
         echo "</p>";
@@ -47,32 +81,7 @@
         echo "</div>";
 
     ?>
-        <form method="post", action="../app/comments/add_comment.php">
-            <input type="text" placeholder="Leave a comment" name="body">
-            <?php 
-                echo "<input type='hidden' name='postId' value='" .  $_GET["post_id"] . "'>";
-            ?>
-            <input type="submit">
-        </form>
-            
-         <form method="post", action="../app/users/add_friend.php">
-         <?php 
-         require_once "../models/User.php";
-         require_once "../models/Post.php";
-
-         session_start();
-         $user = unserialize($_SESSION['user']);
-         $friends = $user->getFriends($user->getId());
-         
-         $post = Post::getPostById($_GET["post_id"]);
-         $userId = $post->getUserId();
-         if(in_array($userId, $friends) || $userId==$user->getId()){
-             return;
-         }
-         echo "<input type='hidden' name='userId' value='" . $userId . "'>"
-         ?>
-         <input type="submit" value="Follow post author">
-        </form>
+      
 
 
 </body>
